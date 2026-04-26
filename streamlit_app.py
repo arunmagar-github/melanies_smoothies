d89b24088ff54eec905288a4068d189a85ad3ac0
Arun Magar <70889447+arunmagar-github@users.noreply.github.com>
2026-04-26 12:20:35 +0530

    Update streamlit_app.py
    chaning hardcode api call to dynamic

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,8 @@ if ingredients_list:
   
   for friut_chosen in ingredients_list:
     ingredients_string+=friut_chosen + ' '
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.subheader(friut_chosen + ' Nutrition Information')
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+friut_chosen)
     sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order) values ('""" + ingredients_string + """','""" + name_on_order + """' )"""
